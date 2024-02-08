f298b0210da8d40990abe267d50aa9af4b3e7bf8
Isaiah Onando Mulang <mulang.onando@gmail.com>
2024-02-08 09:28:05 +0300

    Update main.cpp

--- finddup/main.cpp
+++ finddup/main.cpp
@@ -1,12 +1,15 @@
+/*
+It's good to include here a comment containing the question you are solving and a description of your proposed solution
+    */
 #include <iostream>
 #include <vector>
 #include <unordered_set>
 
 bool containsDuplicate(std::vector<int>& nums) {
-    std::unordered_set<int> seen;
+    std::unordered_set<int> seen; // Very good
 
     for (int num : nums) {
-        if (seen.count(num) > 0) {
+        if (seen.count(num) > 0) {  // Are you sure this line should not be like see.contains(num) OR seen.find(num) ????
             return true;
         }
         seen.insert(num);
